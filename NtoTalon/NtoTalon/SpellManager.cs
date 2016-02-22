8587ce43caabcad43c238b0af32e2c1e0f0714ff
milbradt <milbradt_leandro@hotmail.com>
2016-02-22 18:10:05 -0300

    TalonAddon
    Addon for EB

---
+++ NtoTalon/NtoTalon/SpellManager.cs
@@ -0,0 +1,30 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+
+namespace AddonTemplate
+{
+    public static class SpellManager
+    {
+        // You will need to edit the types of spells you have for each champ as they
+        // don't have the same type for each champ, for example Xerath Q is chargeable,
+        // right now it's  set to Active.
+        public static Spell.Active Q { get; private set; }
+        public static Spell.Skillshot W { get; private set; }
+        public static Spell.Targeted E { get; private set; }
+        public static Spell.Active R { get; private set; }
+
+        static SpellManager()
+        {
+            // Initialize spells
+            Q = new Spell.Active(SpellSlot.Q);
+            W = new Spell.Skillshot(SpellSlot.W, 600, SkillShotType.Cone);
+            E = new Spell.Targeted(SpellSlot.E, 700);
+            R = new Spell.Skillshot(SpellSlot.R, 500, SkillShotType.Circular);
+        }
+
+        public static void Initialize()
+        {
+            // Let the static initializer do the job, this way we avoid multiple init calls aswell
+        }
+    }
+}
